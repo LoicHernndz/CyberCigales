debe6c137345da73d65f38415a08f6aad6fec0e2
Loïc Hernandez <loic.hernandez@etu.univ-amu.fr>
2025-10-03 10:07:43 +0200

    Essai push vers Serveur

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $controller = [new Register(), new RegisterPost(), new Login(), new Homepage()];
 //  AFFICHAGE DU SITE SELON URI
 foreach ($controller as $key => $value) {
     if($value::support($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'])){
-        $value->control();
+        $value->control();  //  Execute l'action du controller
         exit();
     }
 }
