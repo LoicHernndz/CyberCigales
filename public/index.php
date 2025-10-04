22fe1bac2d6d999c7a1d11cc149c134692f03966
Loïc Hernandez <loic.hernandez@etu.univ-amu.fr>
2025-10-04 21:42:28 +0200

    implementation reset password

--- public/index.php
+++ public/index.php
@@ -2,15 +2,19 @@
 include "Autoloader.php";
 include "../src/helpers/session_helper.php";
 
+use Controllers\User\CreateNewPassword;
 use Controllers\User\Login;
 use Controllers\User\LoginPost;
+use Controllers\User\ResetPassword;
+use Controllers\User\ResetPasswordPost;
 use Controllers\User\Signup;
 use Controllers\User\SignupPost;
 use Controllers\User\Logout;
 use Controllers\Homepage;
 
 // LISTE MANUELLE DES CONTROLLERS DISPONIBLES
-$controller = [new Signup(), new SignupPost(), new Login(), new LoginPost(), new Logout(), new Homepage()];
+$controller = [new Signup(), new SignupPost(), new Login(), new LoginPost(), new Logout(),
+    new ResetPassword(), new ResetPasswordPost(), new CreateNewPassword(), new Homepage()];
 
 //  AFFICHAGE DU SITE SELON URI
 foreach ($controller as $key => $value) {
