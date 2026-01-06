985ae156d8d85d82f371fb0e6319997f264a7a1e
Younes Benahmed <younes.benahmed.pro90@gmail.com>
2026-01-06 17:08:01 +0100

    style: Hamming avec style MacOS glassmorphism - CSS complet style Apple (blur, gradients, boutons macOS) - Vue simplifiee pour iframe (pas de header site) - Couleurs SF Design: vert #34c759, bleu #007aff - Animations subtiles et scrollbar macOS - Responsive pour petites fenetres

--- src/Views/Game/Hamming/HammingView.php
+++ src/Views/Game/Hamming/HammingView.php
@@ -52,8 +52,6 @@ class HammingView extends AbstractView
     
     function renderHeader(): void
     {
-        $logoHref = isset($_SESSION['user_id']) ? '/dashboard' : '/';
-        
         echo '
 <!DOCTYPE html>
 <html lang="fr">
@@ -61,13 +59,20 @@ class HammingView extends AbstractView
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hamming Rush - CyberCigales</title>
+        <title>Hamming Rush</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=SF+Pro+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="/styles/main.css?v=5" type="text/css">
         <link rel="icon" href="/images/favicon.svg" type="image/svg+xml">
+        <style>
+            * { box-sizing: border-box; margin: 0; padding: 0; }
+            html, body { 
+                height: 100%; 
+                overflow: hidden;
+                background: linear-gradient(180deg, #f5f5f7 0%, #e8e8ed 100%);
+            }
+        </style>
     </head>
     <body>';
     }
