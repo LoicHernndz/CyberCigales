beae1e46fe3958f25e4fa2abf0c1e60435c22af5
benahmed <younes.benahmed@etu.univ-amu.fr>
2026-01-12 17:11:36 +0100

    docs(controllers): documentation PHPDoc batch final controllers
    - Bash : terminal simulé et exécution commandes
    - Leçons : LeconCesar et autres leçons chiffrement
    - Interfaces : Web, Mail, Agenda (déjà documentées)
    - Autres : DataBreach, Captcha, MacOS, AllCodePage, etc.

--- src/Controllers/Bash/Bash.php
+++ src/Controllers/Bash/Bash.php
@@ -5,8 +5,16 @@ namespace Controllers\Bash;
 use Controllers\AbstractController;
 use Views\Bash\BashView;
 
+/**
+ * Contrôleur de la page du terminal Bash simulé
+ */
 class Bash extends AbstractController
 {
+    /**
+     * Affiche le terminal Bash
+     * 
+     * @return void
+     */
     public function getMethod(): void
     {
         $view = new BashView();
--- src/Controllers/Lecon/LeconCesar.php
+++ src/Controllers/Lecon/LeconCesar.php
@@ -5,9 +5,16 @@ namespace Controllers\Lecon;
 use Views\Lecon\LeconCesar\LeconCesarView;
 use Controllers\AbstractController;
 
+/**
+ * Contrôleur de la leçon sur le chiffrement de César
+ */
 class LeconCesar extends AbstractController {
 
-    // Méthode principale exécutée quand la route correspond à ce contrôleur
+    /**
+     * Affiche la leçon César
+     * 
+     * @return void
+     */
     function getMethod(){
         // Création d’une instance de la vue "MentionsView"
         $view = new LeconCesarView();
